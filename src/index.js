1e5901dde082800c6966f5402c10e497094b213c
Pradeep-kumar-py <pk4401128@gmail.com>
2025-09-27 19:33:58 +0530

    Update CORS allowed origins to include additional local development URLs

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ const api = [
     "http://localhost:8081",
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "http://localhost:3001",
     "https://admin.sniffi.in/",
     "https://www.sniffi.in/"
 
